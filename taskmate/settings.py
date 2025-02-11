933e7cfc0439ff145eb89fbe400df32355bcba40
JThorley11 <jacobjt@btinternet.com>
2025-02-11 20:03:40 +0000

    Remove backslash for CRSF

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -19,8 +19,8 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "https://task-mate.up.railway.app/"]
-CSRF_TRUSTED_ORIGINS = ['https://task-mate.up.railway.app/']
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "https://task-mate.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = ['https://task-mate.up.railway.app']
 
 
 # Application definition
